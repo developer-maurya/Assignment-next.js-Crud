874f12fe28e2caf22556e7d2bd132a2aff1e2441
Chitranjan kumar <chitranjan75075@gmail.com>
2025-12-29 23:25:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import "./globals.css";
+import type { ReactNode } from "react";
+
+export const metadata = {
+  title: "Next.js CRUD",
+  description: "CRUD Assignment",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
 
